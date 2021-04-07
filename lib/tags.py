02ecb86cc8a427697b7fdc799878efe4c14cd3c7
Andreas Backström <mozzo242@gmail.com>
2021-04-07 13:21:59 +0200

    Add library for tag handling

---
+++ lib/tags.py
@@ -0,0 +1,17 @@
+from backend.database import init_db, Files, session
+from backend.utils import flatten
+
+class Tags:
+    def __init__(self):
+        db = init_db()
+        self.session = session(db)
+
+    def get(self, tag):
+        tag = self.session.query(Files).filter(Files.tags.like(tag)).all()
+        return tag
+    
+    def get_all(self):
+        tags = self.session.query(Files.tags).all()
+        out = [item.split(',') for t in tags for item in t]
+        comma_separated = ", ".join(set(flatten(out)))
+        return comma_separated
