3b9aa94ce25ecc3672d2a6ee24b37db9bef0a314
Andreas Backström <mozzo242@gmail.com>
2021-04-07 21:27:21 +0200

    Add created field to property window

--- gui/components/property.py
+++ gui/components/property.py
@@ -11,10 +11,12 @@ class PropertyWindow(QDockWidget):
         hash_item = QTreeWidgetItem(["Hash", info.file_hash])
         location_item = QTreeWidgetItem(["Location", info.file_path])
         tag_item = QTreeWidgetItem(["Tags", info.tags])
+        created_item = QTreeWidgetItem(['Created', str(info.created)])
 
         self.treeview.addTopLevelItem(name_item)
         self.treeview.addTopLevelItem(hash_item)
         self.treeview.addTopLevelItem(location_item)
         self.treeview.addTopLevelItem(tag_item)
+        self.treeview.addTopLevelItem(created_item)
 
         self.setWidget(self.treeview)
