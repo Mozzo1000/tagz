9caffe93fa6214f33459b81f443a78e0be76c8c9
Andreas Backström <mozzo242@gmail.com>
2021-04-07 19:14:47 +0200

    Add refresh button to tag list

--- gui/components/taglist.py
+++ gui/components/taglist.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDockWidget, QListWidget, QListWidgetItem, QMenu, QAction, QLineEdit, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QDockWidget, QListWidget, QListWidgetItem, QMenu, QAction, QLineEdit, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStyle
 from PyQt5.QtCore import Qt
 from lib.tags import Tags
 import csv
@@ -11,6 +11,7 @@ class TagList(QDockWidget):
         self.setWindowTitle('Tags')
         self.main_widget = QWidget(self)
         self.layout = QVBoxLayout(self)
+        self.hlayout = QHBoxLayout(self)
 
         self.listview = QListWidget()
         self.listview.itemDoubleClicked.connect(self.item_clicked)
@@ -21,19 +22,25 @@ class TagList(QDockWidget):
         self.search_input.setPlaceholderText('Search...')
         self.search_input.textChanged.connect(self.search_changed)
 
+        self.refresh_button = QPushButton()
+        self.refresh_button.setIcon(self.style().standardIcon(getattr(QStyle, 'SP_BrowserReload')))
+        self.refresh_button.clicked.connect(self.refresh_listview)
+
 
         self.tags = Tags()
 
         self.populate_list()
         
-        self.layout.addWidget(self.search_input)
+        self.hlayout.addWidget(self.search_input)
+        self.hlayout.addWidget(self.refresh_button)
+        self.layout.addLayout(self.hlayout)
         self.layout.addWidget(self.listview)
 
         self.main_widget.setLayout(self.layout)
         self.setWidget(self.main_widget)
 
     def search_changed(self, event):
-        items = self.listview.findItems(event, Qt.MatchContains)
+        items = self.listview.findItems(event, Qt.MatchRegularExpression)
         all_items = self.listview.findItems('*', Qt.MatchWildcard)
         for item in all_items:
             item.setHidden(True)
